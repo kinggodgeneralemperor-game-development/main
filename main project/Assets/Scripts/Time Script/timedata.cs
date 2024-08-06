3b387d177afd1fb684b78ae299ff40d3dcf8a0b4
yangsonguk11 <yangsonguk11@gmail.com>
2024-08-06 17:43:01 +0900

    시간 수정
    분이 60을 넘어갈 때, 시가 24를 넘어갈 때 시와 일이 넘어가도록 함

--- main project/Assets/Scripts/Time Script/timedata.cs
+++ main project/Assets/Scripts/Time Script/timedata.cs
@@ -8,12 +8,15 @@ public class timedata : MonoBehaviour
     static double prevtime = 0;     //직전 시간
     static int hour;
     static double minute;
+    static int day;
     // Start is called before the first frame update
     void Start()        //time, hour, minute는 세이브 파일 따라서 바꾸기
     {
         prevtime = 0;
         hour = 0;
         minute = 0;
+        day = 0;
+        Update();
     }
 
     // Update is called once per frame
@@ -22,8 +25,19 @@ public class timedata : MonoBehaviour
         minute += (double)Time.time - prevtime;
         if (minute >= 60)
         {
-            hour++;
-            minute -= 60;
+            while (minute >= 60)
+            {
+                hour++;
+                minute -= 60;
+            }
+        }
+        if(hour >= 24)
+        {
+            while(hour >= 24)
+            {
+                day++;
+                hour -= 24;
+            }
         }
         prevtime = (double)Time.time;
     }
