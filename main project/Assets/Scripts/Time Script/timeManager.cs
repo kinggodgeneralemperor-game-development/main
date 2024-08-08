b84c9e2c0f0895d2d05542ac1eaee77498d624b6
treetreet <treetreet@kakao.com>
2024-08-08 14:31:54 +0900

    Revert "Merge branch 'main' of https://github.com/kinggodgeneralemperor-game-development/main"
    This reverts commit 3f957e51e5f813a05988bba3fff31d26869ea9d3, reversing
    changes made to b0ddb30b49061c83499cdf14a011d7b31022a5da.

--- main project/Assets/Scripts/Time Script/timeManager.cs
+++ main project/Assets/Scripts/Time Script/timeManager.cs
@@ -5,18 +5,16 @@ using UnityEngine.UI;
 using TMPro;
 public class timeManager : MonoBehaviour
 {
-    public TextMeshProUGUI maintimeText;       //현재 시간 표시 텍스트
-    public TextMeshProUGUI currentGoldText;    //현재 골드 표시 텍스트
-
+    public TextMeshProUGUI timeText;
+    // Start is called before the first frame update
     void Start()
     {
-        Update();
+        timeText.text = string.Format("{0:0.00}", timedata.Gettime());
     }
 
     // Update is called once per frame
-    void Update()                               //현재 상태를 업데이트 함
+    void Update()
     {
-        maintimeText.text = string.Format("{0:00} : {1:00}", timedata.Gethour(),timedata.Getminute());
-        currentGoldText.text = string.Format("Gold : {0:00}", GoldManager.GetcurrentGold());
+        timeText.text = string.Format("{0:00} : {1:00}", timedata.Gethour(),timedata.Getminute());
     }
 }
